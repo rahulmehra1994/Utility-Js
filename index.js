38b5894a2ddf46a3a2dd308664f4bae37ba9b283
Rahul Mehra <rhlmehra1994@gmail.com>
2018-07-11 15:02:49 +0530

    added removeDuplicates method

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ var jaymeh = {
     
             return result;
         };
+    },
+    removeDuplicates: function(myArr, prop) {
+        return myArr.filter((obj, pos, arr) => {
+            return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+        });
     }
 }
 
